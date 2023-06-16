18e4ceff72fe666d68a92bd17d40db6bf97d6130
Trecia Kat <tksekgalo@gmail.com>
2023-06-16 16:33:01 +0200

    added custom colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#090a0b",
+        primary: "#524f64",
+        secondary: "#f8f6f8",
+        accent: "#5c4f64",
+        cream: "#eeeff2"
+      },
+    },
   },
   plugins: [],
 };
