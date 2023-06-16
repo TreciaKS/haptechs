90c2c415a7d02d5a02e26a92fb78ed8b229535bf
Trecia Kat <tksekgalo@gmail.com>
2023-06-16 12:33:57 +0200

    tested react and tailwind

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Header />
       </header>
     </div>
   );
