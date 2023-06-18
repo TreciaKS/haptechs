25d0cd1c8eebe1dd37526ae9cbd0792109bd43c5
Trecia Kat <tksekgalo@gmail.com>
2023-06-18 21:14:27 +0200

    removed underline

--- src/Layouts/ProductsLayout.js
+++ src/Layouts/ProductsLayout.js
@@ -6,7 +6,7 @@ const ProductsLayout = ({children}) => {
       className="relative w-full max-w-6xl px-10 mx-auto text-gray-800 bg-white md:text-left lg:p-16"
       id="products"
     >
-      <h1 className="mt-5 mb-4 text-5xl text-center underline max-md:text-3xl underline-offset-8">
+      <h1 className="mt-5 mb-4 text-4xl text-center max-md:text-3xl">
         Products
       </h1>
       {children}
